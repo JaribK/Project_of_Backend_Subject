d2b11bc18461d7cfd8b3bfbecf73e2e733a9abb9
Yor-in <141174419+JaribK@users.noreply.github.com>
2024-01-15 20:31:51 +0700

    Update index.js

--- frontend-shopmai/router/index.js
+++ frontend-shopmai/router/index.js
@@ -1,7 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import HomePage from '../src/pages/HomePage.vue'
 
 const routes = [
-
+    {
+        path: '/',
+        name: 'home',
+        component: HomePage
+    },
 ]
 
 const router = createRouter({
